befb1c50184d6f54557d5b8b5142361e9d3e1ab6
Matheus <matheuspl92@hotmail.com>
2022-05-10 17:26:25 -0300

    Add operate() function

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-function add(a, b){return a + b;}
+function add(a, b){return Number(a) + Number(b);}
 
-function subtract(a, b){return a - b;}
+function subtract(a, b){return Number(a) - Number(b);}
 
-function multiply(a, b){return a * b;}
+function multiply(a, b){return Number(a) * Number(b);}
 
-function divide(a, b){return a / b;}
+function divide(a, b){return Number(a) / Number(b);}
+
+function operate(operator, a, b){
+    if(operator === "+"){return add(a, b);}
+    if(operator === "-"){return subtract(a, b);}
+    if(operator === "*"){return multiply(a, b);}
+    if(operator === "/"){return divide(a, b);}
+}
